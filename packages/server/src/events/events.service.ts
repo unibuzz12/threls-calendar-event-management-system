01b8a844a61c492eec3592dbd8bc13673b0145f8
blackberry <angelfirst124@outlook.com>
2023-06-22 22:18:47 +0300

    add events service

---
+++ packages/server/src/events/events.service.ts
@@ -0,0 +1,19 @@
+import { Model } from 'mongoose';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Event } from './schemas/event.schema';
+import { CreateEventDto } from './dto/create-cat.dto';
+
+@Injectable()
+export class EventsService {
+  constructor(@InjectModel(Event.name) private eventModel: Model<Event>) {}
+
+  async create(createEventDto: CreateEventDto): Promise<Event> {
+    const createdEvent = new this.eventModel(createEventDto);
+    return createdEvent.save();
+  }
+
+  async findAll(): Promise<Event[]> {
+    return this.eventModel.find().exec();
+  }
+}
