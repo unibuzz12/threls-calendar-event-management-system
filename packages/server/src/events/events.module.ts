7da9bc64952e7407ffd71f93b56df6c67a637c62
blackberry <angelfirst124@outlook.com>
2023-06-22 22:17:54 +0300

    add events module

---
+++ packages/server/src/events/events.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { EventsService } from './events.service';
+import { EventsController } from './events.controller';
+import { Event, EventSchema } from './schemas/event.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Event.name, schema: EventSchema }]),
+  ],
+  providers: [EventsService],
+  controllers: [EventsController],
+})
+export class EventsModule {}
