cd74b1416e06a896af574b073a4c233c7e012c84
Pro Dev <92237662+black924berry@users.noreply.github.com>
2023-06-22 21:14:51 +0200

    Feature/mongodb-integration

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsModule } from './events/events.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
+    ),
+    EventsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
