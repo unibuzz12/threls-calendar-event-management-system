bc4a2d7d7b2f09047d0902814a8cd590eaed564b
blackberry <angelfirst124@outlook.com>
2023-06-23 21:48:58 +0300

    add selector component

---
+++ packages/client/src/components/Selector/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  MenuItem,
+  Select,
+} from '@mui/material';
+import { ISelector } from "@/utils/interfacesUtil";
+
+const Selector: React.FC<ISelector> = (props) => {
+  const { currentItem, selectList, onSelectChange } = props;
+
+  return (
+    <>
+      <Select
+        value={currentItem}
+        onChange={(e) => onSelectChange(e.target.value)}
+        inputProps={{ 'aria-label': 'Without label' }}
+      >
+        {selectList && selectList.map((item: string | number) =>
+          <MenuItem value={item} key={item}>{item}</MenuItem>
+        )}
+      </Select>
+    </>
+  )
+}
+
+export default Selector;
